a58bea15f3138149a60c448c23074ecb566e8191
Naresh Madiraju <naresh.johnny@gmail.com>
2021-04-28 11:35:30 -0400

    added 5 levels

---
+++ maths/arithmetic/levels.js
@@ -0,0 +1,39 @@
+var LEVELS = {
+  1: {
+    "min": 1,
+    "max": 10,
+    "conditions": []
+  },
+  2: {
+    "min": 1,
+    "max": 99,
+    "conditions": {
+      "additions": [],
+      "subtraction": []
+    }
+  },
+  3: {
+    "min": 11,
+    "max": 999,
+    "conditions": {
+      "additions": [],
+      "subtraction": []
+    }
+  },
+  4: {
+    "min": 11,
+    "max": 9999,
+    "conditions": {
+      "additions": [],
+      "subtraction": []
+    }
+  },
+  5: {
+    "min": 11,
+    "max": 9999,
+    "conditions": {
+      "additions": [],
+      "subtraction": []
+    }
+  }
+}
